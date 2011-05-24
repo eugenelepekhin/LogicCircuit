53dceb6659f92c063555f110058b549c30a7caf8
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2011-05-24 01:36:20 -0700

    Adding ProjectTester so projects can be tested

---
+++ Sources/LogicCircuit.UnitTest/ProjectTester.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LogicCircuit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+
+namespace LogicCircuit.UnitTest {
+	/// <summary>
+	/// Helper class to load and prepare testing of project
+	/// </summary>
+	public class ProjectTester {
+		public CircuitProject CircuitProject { get; private set; }
+		public CircuitMap CircuitMap { get; private set; }
+		public CircuitState CircuitState { get; private set; }
+		public Project Project { get { return this.CircuitProject.ProjectSet.Project; } }
+		public FunctionConstant[] Input { get; private set; }
+		public FunctionProbe[] Output { get; private set; }
+
+		public ProjectTester(TestContext testContext, string projectText, string initialCircuit) {
+			// First save project text to test directory
+			string path = Path.Combine(testContext.TestRunDirectory, "ProjectTester.xml");
+			File.WriteAllText(path, projectText, Encoding.UTF8);
+			// Load if from test directory
+			this.CircuitProject = CircuitProject.Create(path);
+			if(initialCircuit != null) {
+				LogicalCircuit circuit = this.CircuitProject.LogicalCircuitSet.FindByName(initialCircuit);
+				Assert.IsNotNull(circuit, "initial circuit not found in the project");
+				this.CircuitProject.InOmitTransaction(() => this.CircuitProject.ProjectSet.Project.LogicalCircuit = circuit);
+			}
+			// Init App resources so all the visual elements will be able to find them
+			if(App.CurrentApp == null) {
+				App app = new App();
+				if(Application.ResourceAssembly == null) {
+					Application.ResourceAssembly = typeof(App).Assembly;
+					app.InitializeComponent();
+				}
+			}
+			// Create map and state
+			this.CircuitMap = new CircuitMap(this.CircuitProject.ProjectSet.Project.LogicalCircuit);
+			this.CircuitState = this.CircuitMap.Apply(CircuitRunner.HistorySize);
+
+			// Init controlling points of the main circuit: constants and probes
+			List<CircuitSymbol> inputSymbol = new List<CircuitSymbol>();
+			foreach(CircuitSymbol symbol in this.Project.LogicalCircuit.CircuitSymbols()) {
+				if(symbol.Circuit is Constant) {
+					inputSymbol.Add(symbol);
+				}
+			}
+			CircuitSymbolComparer circuitSymbolComparer = new CircuitSymbolComparer(true);
+			inputSymbol.Sort(circuitSymbolComparer);
+			this.Input = inputSymbol.Select(s => (FunctionConstant)this.CircuitMap.Input(s)).ToArray();
+
+			List<CircuitSymbol> outputSymbol = new List<CircuitSymbol>();
+			foreach(CircuitSymbol symbol in this.Project.LogicalCircuit.CircuitSymbols()) {
+				Gate g = symbol.Circuit as Gate;
+				if(g != null && g.GateType == GateType.Probe) {
+					outputSymbol.Add(symbol);
+				}
+			}
+			outputSymbol.Sort(circuitSymbolComparer);
+			this.Output = outputSymbol.Select(s => this.CircuitMap.FunctionProbe(s)).ToArray();
+		}
+	}
+}
---
+++ Sources/LogicCircuit.UnitTest/VisualElementLoadTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using LogicCircuit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicCircuit.UnitTest {
+	/// <summary>
+	/// Test of loading of projects with visual elements like 7 segment display.
+	/// </summary>
+	[TestClass()]
+	public class VisualElementLoadTest {
+
+		#region Additional test attributes
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext { get; set; }
+
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+		/// <summary>
+		/// A test for ProjectTester
+		/// </summary>
+		[TestMethod()]
+		public void VisualElementLoadProjectTesterTest() {
+			ProjectTester tester = new ProjectTester(this.TestContext, Properties.Resources.VisualElementLoadTest, null);
+			Assert.AreEqual(1, tester.Input.Length);
+			Assert.AreEqual(1, tester.Output.Length);
+			Assert.IsTrue(tester.Input.All(f => f != null));
+			Assert.IsTrue(tester.Output.All(f => f != null));
+
+			bool succes = tester.CircuitState.Evaluate(true);
+			Assert.IsTrue(succes);
+			Assert.AreEqual(1, tester.Input[0].Value);
+			Assert.AreEqual(2, tester.Output[0].Pack());
+		}
+	}
+}
