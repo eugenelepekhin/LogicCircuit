1165611f20f83a98e4a5392689b593401b2a9389
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2012-04-29 01:35:19 -0700

    Updating version for release

--- Sources/LogicCircuit/Properties/AssemblyInfo.cs
+++ Sources/LogicCircuit/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.11.8.13")]
-[assembly: AssemblyFileVersion("2.11.8.13")]
+[assembly: AssemblyVersion("2.12.5.1")]
+[assembly: AssemblyFileVersion("2.12.5.1")]
