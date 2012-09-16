d11c952abe50d47445f20c0d9dc1591dc02e492a
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2012-09-16 00:04:09 -0700

    Fixing build warnings

--- Sources/LogicCircuit/Dialog/DialogRAM.cs
+++ Sources/LogicCircuit/Dialog/DialogRAM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace LogicCircuit {
+	[SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "RAM")]
 	public class DialogRAM : DialogMemoryEditor {
 		public DialogRAM(Memory memory) : base(memory) {
 			Tracer.Assert(memory.Writable);
--- Sources/LogicCircuit/Dialog/DialogROM.cs
+++ Sources/LogicCircuit/Dialog/DialogROM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace LogicCircuit {
+	[SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "ROM")]
 	public class DialogROM : DialogMemoryEditor {
 		public DialogROM(Memory memory) : base(memory) {
 			Tracer.Assert(!memory.Writable);
