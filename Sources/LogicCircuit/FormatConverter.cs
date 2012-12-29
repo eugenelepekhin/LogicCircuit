9653df789e0bbd068a2a03d34cf735eafbcd44c7
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2012-12-29 00:30:13 -0800

    Adding format converter and use it to display current zoom

---
+++ Sources/LogicCircuit/FormatConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace LogicCircuit {
+	public class FormatConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			Tracer.Assert(parameter != null && (parameter is string));
+			Tracer.Assert(targetType == typeof(string));
+			return string.Format(App.CurrentCulture, parameter.ToString(), value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			throw new NotSupportedException();
+		}
+	}
+}
