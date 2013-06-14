032b2156c423fae57e0b69c5960e161169f0e821
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2013-06-14 15:59:24 -0700

    Adding workaround for WPF bug that is incorrectly resizes grid if MinHeight is set.

---
+++ Sources/LogicCircuit/DoubleAdderConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LogicCircuit {
+	public class DoubleAdderConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			if(targetType == typeof(double) && value is double && parameter != null) {
+				double delta;
+				if(!double.TryParse(parameter.ToString(), out delta)) {
+					delta = 0;
+				}
+				return Math.Max(0, (double)value + delta);
+			}
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			throw new InvalidOperationException();
+		}
+	}
+}
