1de60d99d917e022be1110d88b8ce5253944f24e
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2015-05-03 18:09:24 -0700

    Adding AutoWidthComboBox and use it main frame.

---
+++ Sources/LogicCircuit/AutoWidthComboBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace LogicCircuit {
+	public class AutoWidthComboBox : ComboBox {
+		public AutoWidthComboBox() {
+			this.Loaded += (object sender, RoutedEventArgs e) => {
+				Popup popup = this.GetTemplateChild("PART_Popup") as Popup;
+				FrameworkElement button = this.GetTemplateChild("toggleButton") as FrameworkElement;
+				if(popup != null) {
+					UIElement child = popup.Child;
+					child.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+					this.MinWidth = Math.Max(this.MinWidth, child.DesiredSize.Width + ((button != null) ? button.DesiredSize.Width : 20));
+				}
+			};
+		}
+	}
+}
