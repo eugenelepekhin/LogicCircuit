2636042d6f312b78e7e64d25d3a57cfe652da491
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2017-03-11 18:08:40 -0800

    Implementing better console UI

--- Sources/LogicCircuit/Dialog/IronPythonConsole.xaml.cs
+++ Sources/LogicCircuit/Dialog/IronPythonConsole.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
 
@@ -38,14 +35,11 @@ namespace LogicCircuit {
 
 		private SettingsWindowLocationCache windowLocation;
 		public SettingsWindowLocationCache WindowLocation { get { return this.windowLocation ?? (this.windowLocation = new SettingsWindowLocationCache(Settings.User, this)); } }
-		private SettingsStringCache historySettings = new SettingsStringCache(Settings.User, "IronPythonConsole.History", null);
 
 		private ScriptEngine scriptEngine;
 		private MemoryStream stdout;
 		private LogWriter writer;
 		private ScriptScope scope;
-		private List<string> history;
-		private int historyIndex;
 
 		private IronPythonConsole(Mainframe mainframe) {
 			this.DataContext = this;
@@ -53,14 +47,12 @@ namespace LogicCircuit {
 
 			this.scriptEngine = Python.CreateEngine();
 			this.stdout = new MemoryStream();
-			this.writer = new LogWriter(this.textBoxLog);
+			this.writer = new LogWriter(this.console);
+			this.console.CommandEnter = this.Execute;
 
 			this.scriptEngine.Runtime.IO.SetOutput(this.stdout, this.writer);
 			this.scope = this.scriptEngine.CreateScope();
 			this.scope.SetVariable("mainframe", mainframe);
-
-			this.history = this.LoadHistory();
-			this.historyIndex = this.history.Count;
 
 			this.scope.ImportModule("clr");
 			this.scriptEngine.Execute("import clr", this.scope);
@@ -73,32 +65,7 @@ namespace LogicCircuit {
 			this.scriptEngine.Execute("from LogicCircuit import *", this.scope);
 
 			this.writer.WriteLine("IronPython " + this.scriptEngine.LanguageVersion.ToString());
-		}
-
-		private List<string> LoadHistory() {
-			string text = this.historySettings.Value;
-			List<string> list = new List<string>();
-			if(!string.IsNullOrEmpty(text)) {
-				string[] item = text.Split(new char[] { '\n' }, Settings.User.MaxRecentFileCount, StringSplitOptions.RemoveEmptyEntries);
-				if(item != null) {
-					HashSet<string> set = new HashSet<string>();
-					foreach(string c in item) {
-						string command = c.Trim();
-						if(set.Add(command)) {
-							list.Add(command);
-						}
-					}
-				}
-			}
-			return list;
-		}
-
-		private void SaveHistory() {
-			StringBuilder text = new StringBuilder();
-			for(int i = Math.Max(0, this.history.Count - Settings.User.MaxRecentFileCount); i < this.history.Count; i++) {
-				text.AppendLine(this.history[i]);
-			}
-			this.historySettings.Value = text.ToString();
+			this.Prompt();
 		}
 
 		protected override void OnClosed(EventArgs e) {
@@ -106,15 +73,14 @@ namespace LogicCircuit {
 			IronPythonConsole.currentConsole = null;
 			this.stdout.Close();
 			this.writer.Close();
-			this.SaveHistory();
 		}
 
-		private void Execute() {
-			string text = this.textBoxCommand.Text.Trim();
-			if(!string.IsNullOrEmpty(text)) {
-				this.textBoxCommand.Clear();
-				this.HistoryAdd(text);
-				this.writer.WriteLine("> " + text);
+		private void Prompt() {
+			this.console.Prompt(">>>");
+		}
+
+		private void Execute(string text) {
+			if(!string.IsNullOrWhiteSpace(text)) {
 				try {
 					dynamic result = this.scriptEngine.Execute(text, this.scope);
 					if(result != null) {
@@ -123,54 +89,7 @@ namespace LogicCircuit {
 				} catch(Exception exception) {
 					this.writer.WriteLine(exception.Message);
 				}
-				this.textBoxLog.ScrollToEnd();
-			}
-		}
-
-		private void HistoryAdd(string text) {
-			this.history.Remove(text);
-			this.history.Add(text);
-			this.historyIndex = this.history.Count;
-		}
-
-		private void HistoryRemove(string text) {
-			this.history.Remove(text);
-			this.historyIndex = this.history.Count;
-		}
-
-		private void HistoryUp() {
-			if(0 < this.historyIndex) {
-				this.historyIndex--;
-				this.textBoxCommand.Text = this.history[this.historyIndex];
-				this.textBoxCommand.SelectAll();
-			}
-		}
-
-		private void HistoryDown() {
-			if(this.historyIndex < this.history.Count - 1) {
-				this.historyIndex++;
-				this.textBoxCommand.Text = this.history[this.historyIndex];
-				this.textBoxCommand.SelectAll();
-			}
-		}
-
-		private void textBoxCommandKeyDown(object sender, KeyEventArgs e) {
-			switch(e.Key) {
-			case Key.Enter:
-				this.Execute();
-				e.Handled = true;
-				break;
-			case Key.Up:
-				this.HistoryUp();
-				e.Handled = true;
-				break;
-			case Key.Down:
-				this.HistoryDown();
-				e.Handled = true;
-				break;
-			case Key.PageUp:
-			case Key.PageDown:
-				break;
+				this.Prompt();
 			}
 		}
 
---
+++ Sources/LogicCircuit/Dialog/ScriptConsole.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace LogicCircuit {
+	public class ScriptConsole : TextBox {
+		public Action<string> CommandEnter { get; set; } = text => {};
+		private int inputStarts = 0;
+
+		private SettingsStringCache historySettings = new SettingsStringCache(Settings.User, "ScriptConsole.History", null);
+		private List<string> history;
+		private int historyIndex;
+
+		public ScriptConsole() {
+			this.IsUndoEnabled = false;
+			this.AcceptsReturn = true;
+			this.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+			this.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+			this.CommandBindings.Add(new CommandBinding(
+				ApplicationCommands.Paste,
+				new ExecutedRoutedEventHandler((sender, e) => {
+					this.Paste();
+				}),
+				new CanExecuteRoutedEventHandler((sender, e) => {
+					if(!this.IsEditAllowed()) {
+						e.CanExecute = false;
+						e.Handled = true;
+					}
+				})
+			));
+
+			this.CommandBindings.Add(new CommandBinding(
+				ApplicationCommands.Cut,
+				new ExecutedRoutedEventHandler((sender, e) => {
+					this.Cut();
+				}),
+				new CanExecuteRoutedEventHandler((sender, e) => {
+					if(!this.IsEditAllowed()) {
+						e.CanExecute = false;
+						e.Handled = true;
+					}
+				})
+			));
+
+			this.history = this.LoadHistory();
+			this.historyIndex = this.history.Count;
+		}
+
+		private List<string> LoadHistory() {
+			List<string> list = new List<string>();
+			string text = this.historySettings.Value;
+			if(!string.IsNullOrEmpty(text)) {
+				string[] item = text.Split(new char[] { '\n' }, Settings.User.MaxRecentFileCount, StringSplitOptions.RemoveEmptyEntries);
+				if(item != null) {
+					HashSet<string> set = new HashSet<string>();
+					foreach(string c in item) {
+						string command = c.Trim();
+						if(set.Add(command)) {
+							list.Add(command);
+						}
+					}
+				}
+			}
+			return list;
+		}
+
+		private void SaveHistory() {
+			StringBuilder text = new StringBuilder();
+			for(int i = Math.Max(0, this.history.Count - Settings.User.MaxRecentFileCount); i < this.history.Count; i++) {
+				text.AppendLine(this.history[i]);
+			}
+			this.historySettings.Value = text.ToString();
+		}
+
+		private void HistoryAdd(string text) {
+			this.history.Remove(text);
+			this.history.Add(text);
+			this.historyIndex = this.history.Count;
+		}
+
+		private void SetCommand(string text) {
+			this.Text = this.Text.Substring(0, this.inputStarts) + text;
+			this.Select(this.inputStarts, text.Length);
+		}
+
+		private void HistoryUp() {
+			if(0 < this.historyIndex) {
+				this.historyIndex--;
+				this.SetCommand(this.history[this.historyIndex]);
+			}
+		}
+
+		private void HistoryDown() {
+			if(this.historyIndex < this.history.Count - 1) {
+				this.historyIndex++;
+				this.SetCommand(this.history[this.historyIndex]);
+			}
+		}
+
+		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e) {
+			base.OnPropertyChanged(e);
+			if(e.Property == TextBox.IsUndoEnabledProperty && this.IsUndoEnabled ||
+				e.Property == TextBox.AcceptsReturnProperty && !this.AcceptsReturn
+			) {
+				throw new InvalidOperationException();
+			}
+		}
+
+		public void Prompt(string promptText) {
+			string text = this.Text;
+			if(0 < text.Length && text[text.Length - 1] != '\n') {
+				this.AppendText("\n");
+			}
+			this.AppendText(promptText);
+			this.ScrollToEnd();
+			this.inputStarts = this.Text.Length;
+			this.Select(this.Text.Length, 0);
+		}
+
+		private bool IsEditAllowed() {
+			return this.inputStarts <= this.SelectionStart;
+		}
+
+		protected override void OnPreviewTextInput(TextCompositionEventArgs e) {
+			if(!this.IsEditAllowed()) {
+				e.Handled = true;
+			} else {
+				base.OnPreviewTextInput(e);
+			}
+		}
+
+		//Left/Right Arrow keys:  Move cursor left or right within text
+		//Up/Down Arrow keys:  Move cursor to previous/next line, maintaining position within line
+		//Ctrl+Left/Right Arrow:  Move to beginning of previous/next word
+		//Ctrl+Up Arrow:  Move to start of text
+		//Ctrl+Down Arrow:  Move to end of text
+		//Shift+Left/Right/Up/Down Arrow:  Move cursor while selecting text
+		//Home key:  Move to beginning of current line
+		//End key:  Move to end of current line
+		//Shift+Home/End:  Select text to beginning/end of current line
+		//Page Up/Down:  Move up/down full page
+		//Insert key:  Toggle Insert/Overwrite mode
+		//Delete key:  Delete character to right of cursor
+		//Backspace key:  Delete character to right of cursor
+		//Ctrl+A:  Select all text
+		//Ctrl+X:  Cut selected text
+		//Ctrl+C:  Copy selected text
+		//Ctrl+V:  Paste text at current position
+		protected override void OnPreviewKeyDown(KeyEventArgs e) {
+			try {
+				string text;
+				switch(e.Key) {
+				case Key.Enter:
+					text = this.Text.Substring(this.inputStarts);
+					this.inputStarts = int.MaxValue;
+					base.OnPreviewKeyDown(e);
+					this.HistoryAdd(text);
+					this.SaveHistory();
+					this.AppendText("\n");
+					this.Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => this.CommandEnter(text)));
+					e.Handled = true;
+					break;
+				case Key.Up:
+					if(this.IsEditAllowed()) {
+						this.HistoryUp();
+						e.Handled = true;
+					} else {
+						base.OnPreviewKeyDown(e);
+					}
+					break;
+				case Key.Down:
+					if(this.IsEditAllowed()) {
+						this.HistoryDown();
+						e.Handled = true;
+					} else {
+						base.OnPreviewKeyDown(e);
+					}
+					break;
+				case Key.Left:
+					if(this.SelectionStart < this.inputStarts || this.inputStarts < this.SelectionStart &&
+						(e.KeyboardDevice.Modifiers != ModifierKeys.Control || !string.IsNullOrWhiteSpace(this.Text.Substring(this.inputStarts, this.SelectionStart - this.inputStarts)))
+					) {
+						base.OnPreviewKeyDown(e);
+					} else {
+						e.Handled = true;
+					}
+					break;
+				case Key.Home:
+					if(this.IsEditAllowed()) {
+						if(e.KeyboardDevice.Modifiers == ModifierKeys.Shift) {
+							this.Select(this.inputStarts, this.SelectionStart - this.inputStarts);
+						} else {
+							this.Select(this.inputStarts, 0);
+						}
+						e.Handled = true;
+					} else {
+						base.OnPreviewKeyDown(e);
+					}
+					break;
+				case Key.Delete:
+					if(this.IsEditAllowed()) {
+						base.OnPreviewKeyDown(e);
+					} else {
+						e.Handled = true;
+					}
+					break;
+				case Key.Back:
+					if(this.inputStarts < this.SelectionStart) {
+						base.OnPreviewKeyDown(e);
+					} else {
+						e.Handled = true;
+					}
+					break;
+				default:
+					base.OnPreviewKeyDown(e);
+					break;
+				}
+			} catch(Exception exception) {
+				App.Mainframe.ReportException(exception);
+			}
+		}
+	}
+}
