8095516ce3edb1155f3dc7b07523e34ff68940f5
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2017-11-26 01:25:36 -0800

    Updating version to released

--- Sources/LogicCircuit/Properties/AssemblyInfo.cs
+++ Sources/LogicCircuit/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.17.10.17")]
-[assembly: AssemblyFileVersion("2.17.10.17")]
+[assembly: AssemblyVersion("2.17.11.26")]
+[assembly: AssemblyFileVersion("2.17.11.26")]
