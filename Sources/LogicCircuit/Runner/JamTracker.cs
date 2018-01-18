743e3b6a67450e1922e23a01f1596895ede3914c
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2018-01-17 17:39:27 -0800

    Fixing another stack overflow crash coused by self connected wires.

---
+++ Sources/LogicCircuit/Runner/JamTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicCircuit {
+	/// <summary>
+	/// Allow to keep list of observed jams during walking through conductors of the circuits.
+	/// </summary>
+	internal class JamTracker {
+		private class JamBit {
+			private readonly CircuitMap map;
+			private readonly Jam jam;
+			private readonly int bit;
+
+			public JamBit(CircuitMap map, Jam jam, int bit) {
+				Tracer.Assert(map.Circuit == jam.CircuitSymbol.LogicalCircuit);
+				Tracer.Assert(0 <= bit && bit < jam.Pin.BitWidth);
+				this.map = map;
+				this.jam = jam;
+				this.bit = bit;
+			}
+
+			public override int GetHashCode() {
+				return this.map.GetHashCode() ^ this.jam.GetHashCode() ^ this.bit;
+			}
+
+			public override bool Equals(object obj) {
+				JamBit other = (JamBit)obj;
+				return this.map == other.map && this.jam == other.jam && this.bit == other.bit;
+			}
+		}
+
+		private HashSet<JamBit> jamConnected = new HashSet<JamBit>();
+
+		public bool WasTracked(CircuitMap map, Jam jam, int bit) {
+			return !this.jamConnected.Add(new JamBit(map, jam, bit));
+		}
+	}
+}
