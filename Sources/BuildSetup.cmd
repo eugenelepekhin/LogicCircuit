0706a8a35c3af91de8a41d71f77731e25d4319bb
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2023-04-28 11:16:31 -0700

    If build failed stop the batch file.

--- Sources/BuildSetup.cmd
+++ Sources/BuildSetup.cmd
@@ -9,9 +9,16 @@ if "%VSCMD_VER%" == "" (
 
 rem Forse building everything, so all the tools are built there.
 msbuild -r -p:Configuration=Release LogicCircuit.sln
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
 dotnet publish LogicCircuit\LogicCircuit.csproj --sc -c Release -r win-x64 -o LogicCircuit\bin\Release_64\Publish
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
+
 dotnet publish LogicCircuit\LogicCircuit.csproj --sc -c Release -r win-x86 -o LogicCircuit\bin\Release_32\Publish
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
 msbuild -r -p:Configuration=Release;Platform=x64 Setup\Setup.wixproj
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
+
 msbuild -r -p:Configuration=Release;Platform=x86 Setup\Setup.wixproj
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
