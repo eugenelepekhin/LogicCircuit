4fa6d1b648345b90ba650690875e89b9c3b9d856
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2023-04-28 02:32:11 -0700

    Make sure the BuildSetup.cmd is running from dev. command prompt. Some clean up of build files.

--- Sources/BuildSetup.cmd
+++ Sources/BuildSetup.cmd
@@ -1,4 +1,13 @@
-rem forst build everything, so all the tools are there.
+@echo off
+
+if "%VSCMD_VER%" == "" (
+	echo This .CMD file must be run from "Developer Command Prompt for VS 2022" or later
+	exit /B
+)
+
+@echo on
+
+rem Forse building everything, so all the tools are built there.
 msbuild -r -p:Configuration=Release LogicCircuit.sln
 
 dotnet publish LogicCircuit\LogicCircuit.csproj --sc -c Release -r win-x64 -o LogicCircuit\bin\Release_64\Publish
