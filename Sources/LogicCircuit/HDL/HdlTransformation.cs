791cb54116a709d591a08cf51a4e11f1d4eaafef
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2024-04-09 00:26:06 -0700

    More refactoring

--- Sources/LogicCircuit/HDL/HdlTransformation.cs
+++ Sources/LogicCircuit/HDL/HdlTransformation.cs
@@ -5,13 +5,15 @@ using System.Linq;
 
 namespace LogicCircuit {
 	public abstract class HdlTransformation : T4Transformation {
+		public string Name { get; }
 		public IEnumerable<HdlSymbol> InputPins { get; }
 		public IEnumerable<HdlSymbol> OutputPins { get; }
 		public IEnumerable<HdlSymbol> Parts { get; }
 		public bool HasInputPins => this.InputPins.Any();
 		public bool HasOutputPins => this.OutputPins.Any();
 
-		protected HdlTransformation(IEnumerable<HdlSymbol> inputPins, IEnumerable<HdlSymbol> outputPins, IEnumerable<HdlSymbol> parts) {
+		protected HdlTransformation(string name, IEnumerable<HdlSymbol> inputPins, IEnumerable<HdlSymbol> outputPins, IEnumerable<HdlSymbol> parts) {
+			this.Name = name;
 			this.InputPins = inputPins;
 			this.OutputPins = outputPins;
 			this.Parts = parts;
--- Sources/LogicCircuit/HDL/N2THdl.Properties.cs
+++ Sources/LogicCircuit/HDL/N2THdl.Properties.cs
@@ -1,38 +1,30 @@
 ﻿// Ignore Spelling: Hdl
 
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Linq;
 using System.Text;
 
 namespace LogicCircuit {
 	partial class N2THdl {
-		public string Name { get; }
-
-		[SuppressMessage("Performance", "CA1851:Possible multiple enumerations of 'IEnumerable' collection")]
 		private static string PinsText(IEnumerable<HdlSymbol> pins) {
 			StringBuilder text = new StringBuilder();
-			if(pins.Any()) {
-				bool comma = false;
-				foreach(HdlSymbol symbol in pins) {
-					if(comma) {
-						text.Append(", ");
-					} else {
-						comma = true;
-					}
-					Pin pin = (Pin)symbol.CircuitSymbol.Circuit;
-					text.Append(pin.Name);
-					if(1 < pin.BitWidth) {
-						text.AppendFormat(CultureInfo.InvariantCulture, "[{0}]", pin.BitWidth);
-					}
+			bool comma = false;
+			foreach(HdlSymbol symbol in pins) {
+				if(comma) {
+					text.Append(", ");
+				} else {
+					comma = true;
+				}
+				Pin pin = (Pin)symbol.CircuitSymbol.Circuit;
+				text.Append(pin.Name);
+				if(1 < pin.BitWidth) {
+					text.AppendFormat(CultureInfo.InvariantCulture, "[{0}]", pin.BitWidth);
 				}
 			}
 			return text.ToString();
 		}
 
-		public N2THdl(string name, IEnumerable<HdlSymbol> inputPins, IEnumerable<HdlSymbol> outputPins, IEnumerable<HdlSymbol> parts) : base(inputPins, outputPins, parts) {
-			this.Name = name;
+		public N2THdl(string name, IEnumerable<HdlSymbol> inputPins, IEnumerable<HdlSymbol> outputPins, IEnumerable<HdlSymbol> parts) : base(name, inputPins, outputPins, parts) {
 		}
 	}
 }
