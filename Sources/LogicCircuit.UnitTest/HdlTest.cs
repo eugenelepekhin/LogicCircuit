77a1fd5894c1f00e13c21a00619a1f5ca6eb85a3
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2024-04-16 17:30:52 -0700

    Adding more test and include circuit project file in the test sources.

--- Sources/LogicCircuit.UnitTest/HdlTest.cs
+++ Sources/LogicCircuit.UnitTest/HdlTest.cs
@@ -1,12 +1,43 @@
-﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+using System.Xml;
 using LogicCircuit.UnitTest.HDL;
 using Microsoft.VisualStudio.TestTools.UnitTesting.STAExtensions;
 
 namespace LogicCircuit.UnitTest {
 	[TestClass]
+	[DeploymentItem(@"Properties\HDLTests.CircuitProject")]
 	public class HdlTest {
 		public TestContext TestContext { get; set; }
+
+		private void Message(string message) {
+			this.TestContext.WriteLine(message);
+			//Debug.WriteLine(message);
+		}
+
+		private CircuitProject LoadCircuitProject() {
+			string file = Path.Combine(this.TestContext.TestRunDirectory, "Out", "HDLTests.CircuitProject");
+			CircuitProject project = ProjectTester.Load(this.TestContext, File.ReadAllText(file), null);
+			return project;
+		}
+
+		private string HdlFolder() {
+			string path = Path.Combine(this.TestContext.TestRunDirectory, this.TestContext.TestName);
+			if(!Directory.Exists(path)) {
+				Directory.CreateDirectory(path);
+			}
+			return path;
+		}
+
+		private void SortByNote(List<LogicalCircuit> circuits) {
+			double Number(string text) {
+				Match match = Regex.Match(text, @"[0-9]+(\.[0-9]+)?");
+				if(match.Success) {
+					return double.Parse(match.Value);
+				}
+				return int.MaxValue;
+			}
+			circuits.Sort((a, b) => Math.Sign(Number(a.Note) - Number(b.Note)));
+		}
 
 		[TestMethod]
 		public void TestParser() {
@@ -39,28 +70,12 @@ namespace LogicCircuit.UnitTest {
 			}
 		}
 
-		private void Message(string message) {
-			this.TestContext.WriteLine(message);
-			//Debug.WriteLine(message);
-		}
-
 		[STATestMethod]
-		public void TestOverall() {
-			double Number(string text) {
-				Match match = Regex.Match(text, @"[0-9]+(\.[0-9]+)?");
-				if(match.Success) {
-					return double.Parse(match.Value);
-				}
-				return int.MaxValue;
-			}
-			string lcFile = """C:\Projects\CircuitProjects\HDLTests.CircuitProject""";
-			CircuitProject project = ProjectTester.Load(this.TestContext, File.ReadAllText(lcFile), null);
-			string hdlPath = Path.Combine(this.TestContext.TestRunDirectory, this.TestContext.TestName);
-			if(!Directory.Exists(hdlPath)) {
-				Directory.CreateDirectory(hdlPath);
-			}
+		public void HdlTestTruthTables() {
+			CircuitProject project = this.LoadCircuitProject();
+			string hdlPath = this.HdlFolder();
 			List<LogicalCircuit> circuits = project.LogicalCircuitSet.Where(c => c.Category == "Test").ToList();
-			circuits.Sort((a, b) => Math.Sign(Number(a.Note) - Number(b.Note)));
+			this.SortByNote(circuits);
 			foreach(LogicalCircuit circuit in circuits) {
 				ProjectTester.SwitchTo(project, circuit.Name);
 				N2TExport export = new N2TExport(false, false, this.Message, this.Message);
@@ -77,6 +92,37 @@ namespace LogicCircuit.UnitTest {
 
 				bool same = TruthState.AreEqual(lcTable, hdlTable);
 				Assert.IsTrue(same);
+			}
+		}
+
+		[STATestMethod]
+		public void HdlTestErrors() {
+			string text = null;
+			void error(string message) {
+				this.Message(message);
+				text = message;
+			}
+			CircuitProject project = this.LoadCircuitProject();
+			string hdlPath = this.HdlFolder();
+			List<LogicalCircuit> circuits = project.LogicalCircuitSet.Where(c => c.Category == "Error").ToList();
+			this.SortByNote(circuits);
+			foreach(LogicalCircuit circuit in circuits) {
+				ProjectTester.SwitchTo(project, circuit.Name);
+				N2TExport export = new N2TExport(false, false, this.Message, error);
+				bool success = export.ExportCircuit(circuit, hdlPath, false);
+				Assert.IsFalse(success);
+
+				TextNote note = circuit.TextNotes().FirstOrDefault();
+				Assert.IsNotNull(note);
+				if(note != null) {
+					XmlDocument xml = XmlHelper.LoadXml(note.Note);
+					XmlNode node = xml.DocumentElement.FirstChild;
+					Assert.IsNotNull(node);
+					if(node != null) {
+						string pattern = node.InnerText;
+						Assert.IsTrue(Regex.IsMatch(text, pattern));
+					}
+				}
 			}
 		}
 	}
