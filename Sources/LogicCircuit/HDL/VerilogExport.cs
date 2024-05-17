eb23a8a96497950e571271a6a9f55cc4f12982f3
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2024-05-16 17:27:00 -0700

    Adding complete list of keywords for Verilog

--- Sources/LogicCircuit/HDL/VerilogExport.cs
+++ Sources/LogicCircuit/HDL/VerilogExport.cs
@@ -10,10 +10,17 @@ namespace LogicCircuit {
 	internal class VerilogExport : HdlExport {
 		private readonly Regex identifier = new Regex(@"^[a-zA-Z_][a-zA-Z0-9_$]*$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
 		private readonly HashSet<string> keywords = new HashSet<string>() {
-			"module", "input", "output", "inout", "endmodule", "wire", "wand", "wor", "reg", "integer", "time", "real",
-			"realtime", "event", "task", "begin", "end", "endtask", "function", "endfunction", "assign", "deassign",
-			"initial", "always", "defparam", "strength", "delay", "if", "else", "case", "casex", "casez", "endcase", "endcase",
-			"repeat", "while", "for", "fork", "join", "specify", "endspecify", 
+			"always", "and", "assign", "attribute", "begin", "buf", "bufif0", "bufif1", "case", "casex", "casez",
+			"cmos", "deassign", "default", "defparam", "disable", "edge", "else", "end", "endattribute", "endcase",
+			"endfunction", "endmodule", "endprimitive", "endspecify", "endtable", "endtask", "event", "for", "force",
+			"forever", "fork", "function", "highz0", "highz1", "if", "ifnone", "initial", "inout", "input", "integer",
+			"join", "medium", "module", "large", "macromodule", "nand", "negedge", "nmos", "nor", "not", "notif0",
+			"notif1", "or", "output", "parameter", "pmos", "posedge", "primitive", "pull0", "pull1", "pulldown",
+			"pullup", "rcmos", "real", "realtime", "reg", "release", "repeat", "rnmos", "rpmos", "rtran", "rtranif0",
+			"rtranif1", "scalared", "signed", "small", "specify", "specparam", "strength", "strong0", "strong1",
+			"supply0", "supply1", "table", "task", "time", "tran", "tranif0", "tranif1", "tri", "tri0", "tri1",
+			"triand", "trior", "trireg", "unsigned", "vectored", "wait", "wand", "weak0", "weak1", "while", "wire",
+			"wor", "xnor", "xor",
 		};
 
 		public VerilogExport(bool exportTests, bool commentPoints, Action<string> logMessage, Action<string> logError) : base(exportTests, commentPoints, logMessage, logError) {
