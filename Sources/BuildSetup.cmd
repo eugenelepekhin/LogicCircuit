72e8c7fad857717a9c0c4bac20410b6f4bfb2291
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2024-05-28 20:46:43 -0700

    Moving all results of build setup to one folder and adding zips of standalone builds.

--- Sources/BuildSetup.cmd
+++ Sources/BuildSetup.cmd
@@ -11,11 +11,15 @@ rem Force building everything, so all the tools are built there.
 msbuild -r -p:Configuration=Release LogicCircuit.sln
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
+rem Publish for 64 bits
 dotnet publish LogicCircuit\LogicCircuit.csproj --sc -c Release -r win-x64 -o LogicCircuit\bin\Release_64\Publish
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
+rem Publish for 32 bits
 dotnet publish LogicCircuit\LogicCircuit.csproj --sc -c Release -r win-x86 -o LogicCircuit\bin\Release_32\Publish
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
+
+rem Buiding Setup
 
 msbuild -r -p:Configuration=Release;Platform=x64 Setup\Setup.wixproj
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
@@ -23,5 +27,10 @@ msbuild -r -p:Configuration=Release;Platform=x64 Setup\Setup.wixproj
 msbuild -r -p:Configuration=Release;Platform=x86 Setup\Setup.wixproj
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
-msbuild -t:ZipSetup Setup
+rem Buiding Setup Zip files
+
+msbuild -t:ZipSetup Setup -p:Platform=x64
+@if "%ERRORLEVEL%" NEQ "0" (exit /B)
+
+msbuild -t:ZipSetup Setup -p:Platform=x86
 @if "%ERRORLEVEL%" NEQ "0" (exit /B)
